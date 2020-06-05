a038a1978d3f6639fcd304e436c40e8552bc84d9
harrydehix <harry.holly02@gmail.com>
2020-06-05 21:01:28 +0200

    Add basic gui structure

--- eclipseProject/src/application/Main.java
+++ eclipseProject/src/application/Main.java
@@ -12,13 +12,15 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,400,600);
 			
-			Label l = new Label("Hello world!");
-			root.setTop(l);
+			root.setTop(null);
+			
+			root.setBottom(null);
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
