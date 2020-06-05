9be84b930eb6ec71ec3c1df2ea1aa2dcba41c927
BeneCodecademy <dareb.bene@gmail.com>
2020-06-05 20:38:05 +0200

    Add some hello-world-stuff, yeahgit statusgit status

--- eclipseProject/src/application/Main.java
+++ eclipseProject/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 
@@ -12,6 +13,10 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+			
+			Label l = new Label("Hello world!");
+			root.setTop(l);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
