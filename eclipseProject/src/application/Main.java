a836008938e8d15aa600470dd3ee513351f5940a
BeneCodecademy <dareb.bene@gmail.com>
2020-06-05 21:33:10 +0200

    Add whole top of backend

--- eclipseProject/src/application/Main.java
+++ eclipseProject/src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 	
+import backend.ModuloUtils;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 
 public class Main extends Application {
@@ -14,7 +17,38 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,600);
 			
-			root.setTop(null);
+			
+			//TOP
+			
+			//objects
+			HBox inputField = new HBox();
+			TextField base = new TextField();
+			Label operator = new Label("%");
+			TextField module = new TextField();
+			Label equals = new Label("=");
+			Label result = new Label("result");
+			
+			//modulo
+			ModuloUtils op = new ModuloUtils();
+			
+//			//default values -> solved through send-button
+//			int ba = 3;
+//			int mo = 3;
+//			if (!base.getText().equals("")) {
+//				ba = Integer.parseInt(base.getText());
+//			}
+//			if (!module.getText().equals("")) {
+//				mo = Integer.parseInt(module.getText());
+//			}
+			
+//			int res = ModuloUtils.modulo(ba, mo);
+//			result.setText(String.valueOf(res));
+			
+			//add objects
+			inputField.getChildren().addAll(base, operator, module, equals, result);
+			
+			root.setTop(inputField);
+			
 			
 			root.setBottom(null);
 			
